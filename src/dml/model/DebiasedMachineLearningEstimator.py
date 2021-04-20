MSE is correctly computed and reported and also fixed the sklearn-scaling for relevant methods

src/dml/model/DebiasedMachineLearningEstimator.py
@@ -13,7 +13,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from scipy.stats import randint
 
 
@@ -28,9 +28,16 @@ class DML:
         self.verbose = verbose
         self.small_dml = small_dml
 
-    def __add__(self, other):
-        # TODO: Allow for easy consolidation of multipled DML object with smaller experiments for jobs in multiple batches
-        pass
+        self.rmse_y = False
+        self.rmse_t = False
+
+        self._is_estimated = False
+
+    def get_dml_rmse(self):
+        if self._is_estimated:
+            return self.rmse_y, self.rmse_t
+        else:
+            raise RuntimeError('Treatment effect not yet estimated.')
 
     def treatment_effect(self, X=None, y=None, t=None):
         self.X_ = X
@@ -38,7 +45,9 @@ class DML:
         self.t_ = t
 
         fold_seeds = randint.rvs(0, 1000, size=self.n_splits, random_state=0).tolist()
-        treatment_effect = self._dml_estimation(fold_seeds)
+        treatment_effect= self._dml_estimation(fold_seeds)
+        self._is_estimated = True
+
         return treatment_effect
 
     def _dml_estimation(self, fold_seeds):
@@ -52,11 +61,18 @@ class DML:
                             prefer='threads')
         dml_splits = parallel(delayed(self._parallel_estimate_single_split)(X_, y_, t_, i, fold_seeds[i])
                               for i in range(self.n_splits))
+        dml_splits = np.array(dml_splits)
 
-        return np.array(dml_splits).mean()
+        treamtnet_effect = dml_splits[:, 0].mean()
+        self.rmse_y, self.rmse_t = dml_splits[:, 1], dml_splits[:, 2]
+
+        return treamtnet_effect
 
     def _parallel_estimate_single_split(self, X, y, t, split_idx, fold_seed):
         folds = KFold(n_splits=self.n_folds, shuffle=True, random_state=fold_seed)
+
+        rmse_y = 0
+        rmse_t = 0
 
         TE = 0
         y_pool = []
@@ -67,8 +83,8 @@ class DML:
             X_main, y_main, t_main = X[main], y[main], t[main]
             X_aux, y_aux, t_aux = X[aux], y[aux], t[aux]
 
-            y_resid = self._debiased_residuals(X_main, y_main, X_aux, y_aux, self.model_y)
-            t_resid = self._debiased_residuals(X_main, t_main, X_aux, t_aux, self.model_t)
+            y_resid, error_y = self._debiased_residuals(X_main, y_main, X_aux, y_aux, self.model_y)
+            t_resid, error_t = self._debiased_residuals(X_main, t_main, X_aux, t_aux, self.model_t)
 
             y_pool += y_resid.tolist()
             t_pool += t_resid.tolist()
@@ -76,6 +92,8 @@ class DML:
             theta_hat = ols(t_resid, y_resid).item()
 
             TE += theta_hat/self.n_folds
+            rmse_y += error_y/self.n_folds
+            rmse_t += error_t/self.n_folds
 
         if self.small_dml:
             TE = ols(np.array(t_pool), np.array(y_pool))
@@ -83,7 +101,7 @@ class DML:
         if self.verbose == 1:
             print('Split {}/{} Completed'.format(split_idx + 1, self.n_splits))
 
-        return TE
+        return TE, rmse_y, rmse_t
 
     def _debiased_residuals(self, X_main, y_main, X_aux, y_aux, method):
         # Fit on auxiliary sample
@@ -96,7 +114,11 @@ class DML:
 
         # Compute Debiased Residuals || Y - E[Y|X] or D - E[D|X]
         residuals = y_main - y_pred
-        return residuals
+
+        # Compute MSE
+        rmse = np.sqrt(((y_pred-y_aux)**2).mean(axis=0))
+
+        return residuals, rmse
 
     def _ml_estimator(self, X, y, method: str) -> BaseEstimator:
         # TODO Set params per method
@@ -107,11 +129,13 @@ class DML:
         else:
             model = eval('_ml_' + method + '(X, y)')
 
-        if method in ['Lasso', 'Ridge', 'Elastic Net', 'Neural Network']:
-            estimator = Pipeline([('scaler', StandardScaler()),
+        if method in ['Lasso', 'Ridge', 'Elastic_Net', 'Neural_Network']:
+            scaler = MinMaxScaler() if method == 'Neural_Network' else StandardScaler()
+            estimator = Pipeline([('scaler', scaler),
                                   ('model', model)])
         else:
             estimator = model
+
         estimator.fit(X, y)
 
         return estimator

src/dml/model/mlestimators.py
@@ -3,6 +3,11 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import ElasticNetCV, RidgeCV, ElasticNet
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
+from sklearn.exceptions import ConvergenceWarning
+
+from warnings import filterwarnings
+
+filterwarnings(action='ignore', category=ConvergenceWarning)
 
 # from keras.models import Sequential
 # from keras.layers.core import Dense, Activation
@@ -20,8 +25,8 @@ def _ml_Tree(X, y, tune=True):
                                   min_samples_split=round(30/3),
                                   ccp_alpha=0)
     if tune:
-        min_samples_leaf = [0.2, 0.3, 0.4, 0.49]
-        kfold = KFold(n_splits=5, shuffle=True, random_state=0)
+        min_samples_leaf = [0.1, 0.2, 0.3, 0.4, 0.49]
+        kfold = KFold(n_splits=10, shuffle=True, random_state=0)
         cv = GridSearchCV(estimator=model,
                           param_grid={'min_samples_leaf': min_samples_leaf},
                           return_train_score=False,
@@ -32,7 +37,7 @@ def _ml_Tree(X, y, tune=True):
     return model
 
 def _ml_Forest(X, y, tune=False):
-    model = RandomForestRegressor(n_estimators=100, n_jobs=1) # 1.69, 2.01
+    model = RandomForestRegressor(n_estimators=100, n_jobs=1, min_samples_leaf=5) # 1.69, 2.01
     # model = lgbm.LGBMRegressor(boosting_type='rf',
     #                            n_estimators=100,
     #                            feature_fraction= 0.3, # np.sqrt(X.shape[1]) / X.shape[1],
@@ -68,19 +73,18 @@ def _ml_Neural_Network(X, y, tune=True):
     # standardization
     # def single_hidden_layer():
     #     perceptron = Sequential()
-    #     perceptron.add(Dense(10, input_dim=X.shape[1], kernel_initializer='normal',
-    #                    kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
+    #     perceptron.add(Dense(10, input_dim=X.shape[1], kernel_initializer='normal'))
     #     perceptron.add(Dense(1,  kernel_initializer='normal'))
     #     perceptron.compile(loss='mean_squared_error', optimizer='adam')
     #     return perceptron
     # model = KerasRegressor(build_fn=single_hidden_layer, nb_epoch=100, batch_size=30, verbose=0)
-    model = MLPRegressor(hidden_layer_sizes=(10, ), solver='lbfgs',
+    model = MLPRegressor(hidden_layer_sizes=(12, ), solver='lbfgs',
                          max_iter=1200, learning_rate='adaptive', alpha=0.005)
     return model
 
 
 def _ml_Elastic_Net(X, y, tune=True, l1_ratios=None):
-    kfold = KFold(n_splits=4, shuffle=True, random_state=0)
+    kfold = KFold(n_splits=10, shuffle=True, random_state=0)
 
     if tune:
         if l1_ratios is None: